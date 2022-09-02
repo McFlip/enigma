f7549fb7c141c28e08adce629b75f7196df762b7
McFlip <gradydenton@yahoo.com>
2022-09-01 23:13:17 -0400

    marshal key

--- getKey/main.go
+++ getKey/main.go
@@ -1,3 +1,8 @@
+// Extract a private key from a p12 container and save to a PKCS8 file
+// I use youmark's pkcs8 lib because the standard x509 lib
+// doesn't allow for marshalling with a password.
+// For security, we won't store private keys to disk in plain text.
+
 package main
 
 import (
@@ -5,7 +10,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 
+	"github.com/youmark/pkcs8"
 	"software.sslmate.com/src/go-pkcs12"
 )
 
@@ -15,8 +22,8 @@ func main() {
 	}
 	inP12 := os.Args[1]
 	inPW := os.Args[2]
-	// outPW := os.Args[3]
-	// outDir := os.Args[4]
+	outPW := os.Args[3]
+	outDir := os.Args[4]
 
 	p12Bs, err := os.ReadFile(inP12)
 	if err != nil {
@@ -26,11 +33,18 @@ func main() {
 	if err != nil {
 		log.Fatal("Can't decode p12")
 	}
-	// fmt.Println(key)
 	err = key.(*rsa.PrivateKey).Validate()
 	if err != nil {
 		log.Fatal("Key invalid")
 	}
+	keyBs, err := pkcs8.ConvertPrivateKeyToPKCS8(key, []byte(outPW))
+	if err != nil {
+		log.Fatal("Can't convert key to PKCS8")
+	}
 	serial := fmt.Sprintf("%x", cert.SerialNumber)
 	fmt.Println(serial)
+	err = os.WriteFile(filepath.Join(outDir, serial+".key"), keyBs, 0550)
+	if err != nil {
+		log.Fatal("Can't save key to outDir")
+	}
 }
