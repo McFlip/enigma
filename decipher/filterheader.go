cd3bc5050fded2d227148522294d4176168500d2
McFlip <gradydenton@yahoo.com>
2022-09-08 22:08:56 -0400

    filter headers; number output eml

--- decipher/filterheader.go
+++ decipher/filterheader.go
@@ -10,9 +10,6 @@ import (
 )
 
 func filterHeaders(headers string) string {
-	// fmt.Println("@@@@@@@@@@@@@@@@@@@@@")
-	// fmt.Println(headers)
-	// fmt.Println("@@@@@@@@@@@@@@@@@@@@@")
 	// null bytes in MS Outlook string encoding breaks strings funcs & regexp
 	headersBS := []byte(headers)
 	headersBS = bytes.ReplaceAll(headersBS, []byte{00}, []byte{})
@@ -21,24 +18,18 @@ func filterHeaders(headers string) string {
 	currentHeader := ""
 	headerList := strings.Split(headers, "\n")
 	headKey := regexp.MustCompile(`^[A-Z,a-z,-]+:`)
-	throwaway := false
-
 	for _, ln := range headerList {
 		// filter out the keys we don't want
-		if strings.HasPrefix(ln, "Content-Type:") || strings.HasPrefix(ln, "Content-Transfer-Encoding:") || strings.HasPrefix(ln, "Content-Disposition:") {
-			throwaway = true
+		if strings.HasPrefix(ln, "Content-Type:") || strings.HasPrefix(ln, "Content-Transfer-Encoding:") {
 			continue
 		}
 		// Keys can have values accross multiple lines, starting with a space
 		// If we find a new key then we know we are done processing the previous key
 		if headKey.MatchString(ln) {
 			accumulated = accumulated + currentHeader
-			throwaway = false
 			currentHeader = ln + "\n"
 		} else {
-			if !throwaway {
-				currentHeader = currentHeader + ln + "\n"
-			}
+			currentHeader = currentHeader + ln + "\n"
 		}
 	}
 	// Reached the end of the header, flush the last key-value
