a3b671c96fd63e9438d5b3188cde176e15877b82
McFlip <gradydenton@yahoo.com>
2023-05-03 14:23:01 -0400

    refactor proc msg in parallel

--- getSigs/main.go
+++ getSigs/main.go
@@ -60,7 +60,6 @@ func main() {
 	}
 	for completedFiles := 0; completedFiles < len(files); completedFiles++ {
 		currMsg := <-c
-		// log.Println(currMsg)
 		for currMsg != "---END---" {
 			// composite key on serial # and CA strings
 			serial, ca, found := "", "", false
@@ -85,7 +84,6 @@ func main() {
 			key := ca + serial
 			allCerts[key] = "\n----------\n" + currMsg
 			currMsg = <-c
-			// log.Println(currMsg)
 		}
 	}
 	// write out the allCerts.txt file
@@ -119,16 +117,12 @@ func processPST(file string, c chan string) {
 		}
 	}()
 
-	// fmt.Printf("Parsing file...")
-
 	isValidSignature, err := pstFile.IsValidSignature()
-
 	if err != nil {
 		fmt.Printf("Failed to read signature: %s\n", err)
 		c <- "---END---"
 		return
 	}
-
 	if !isValidSignature {
 		fmt.Printf("Invalid file signature.\n")
 		c <- "---END---"
@@ -136,29 +130,20 @@ func processPST(file string, c chan string) {
 	}
 
 	formatType, err := pstFile.GetFormatType()
-
 	if err != nil {
 		fmt.Printf("Failed to get format type: %s\n", err)
 		c <- "---END---"
 		return
 	}
 
-	// fmt.Printf("Format type: %s\n", formatType)
-
 	encryptionType, err := pstFile.GetEncryptionType(formatType)
-
 	if err != nil {
 		fmt.Printf("Failed to get encryption type: %s\n", err)
 		c <- "---END---"
 		return
 	}
 
-	// fmt.Printf("Encryption type: %s\n", encryptionType)
-
-	// fmt.Printf("Initializing B-Trees...\n")
-
 	err = pstFile.InitializeBTrees(formatType)
-
 	if err != nil {
 		fmt.Printf("Failed to initialize node and block b-tree.\n")
 		c <- "---END---"
@@ -183,6 +168,149 @@ func processPST(file string, c chan string) {
 	c <- "---END---"
 }
 
+// Go Routine processes 1 message
+func processMsg(msg pst.Message, pstFile pst.File, formatType string, encryptionType string, c chan string) {
+	// we're only interested in the smime.p7m attachment
+	hasAttachments, err := msg.HasAttachments()
+	if err != nil {
+		log.Println(err)
+		c <- ""
+	}
+	if !hasAttachments {
+		c <- ""
+	}
+
+	from, err := msg.GetFrom(&pstFile, formatType, encryptionType)
+	if err != nil {
+		log.Println(err)
+		c <- ""
+	}
+
+	myAttachments, err := msg.GetAttachments(&pstFile, formatType, encryptionType)
+	if err != nil {
+		log.Println(err)
+		c <- ""
+	}
+
+	for _, attachment := range myAttachments {
+		// check MIME type
+		mimeType, _ := attachment.GetString(14094)
+		if mimeType != "multipart/signed" {
+			continue
+		}
+
+		// Read the attachment
+		attachStream, err := attachment.GetInputStream(&pstFile, formatType, encryptionType)
+		if err != nil {
+			log.Println(err)
+			c <- ""
+		}
+		attachBytes, err := attachStream.ReadCompletely()
+		if err != nil {
+			log.Println(err)
+			c <- ""
+		}
+		msg, err := mail.ReadMessage(bytes.NewReader(attachBytes))
+		if err != nil {
+			log.Println(err)
+			c <- ""
+		}
+
+		// Iterate through msg parts until we get to the smime.p7s
+		_, params, err := mime.ParseMediaType(msg.Header.Get("Content-Type"))
+		if err != nil {
+			log.Println(err)
+			c <- ""
+		}
+		mr := multipart.NewReader(msg.Body, params["boundary"])
+		eof := false
+		for !eof {
+			p, err := mr.NextPart()
+			if err == io.EOF {
+				eof = true
+				continue
+			}
+			if err != nil {
+				log.Fatal(err)
+			}
+			slurp, err := io.ReadAll(p)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if p.Header.Get("Content-Type") == "application/pkcs7-signature; name=\"smime.p7s\"" {
+				// parse the pkcs7 struct
+				dst := make([]byte, len(slurp))
+				n, err := base64.StdEncoding.Decode(dst, slurp)
+				if err != nil {
+					log.Println(err)
+					c <- ""
+				}
+				dst = dst[:n]
+				p7m, err := pkcs7.Parse(dst)
+				if err != nil {
+					log.Println(err)
+					c <- ""
+				}
+
+				// Get the signer info - this is the main objective!
+				signer := p7m.GetOnlySigner()
+
+				// Common Name is in the form LAST.FIRST.MIDDLE.EDIPI
+				cn := signer.Subject.CommonName
+				log.Println(cn)
+				cnSplit := strings.Split(cn, ".")
+				lName := cnSplit[0]
+				fName := cnSplit[1]
+				mName := ""
+				edipi := cnSplit[len(cnSplit)-1]
+				if len(cnSplit) == 4 {
+					mName = cnSplit[2]
+				}
+
+				// get email address directly from the cert, if doesn't exist fall back to FROM field in email
+				email := strings.Join(signer.EmailAddresses, ";")
+
+				// Principle Name, if blank then form from Common Name
+				upn := strings.Join(signer.DNSNames, ";")
+				if len(upn) == 0 {
+					upn = fmt.Sprintf("%s@mil", edipi)
+				}
+
+				serial := signer.SerialNumber
+				issuer := signer.Issuer
+				ca := signer.Issuer.CommonName
+
+				// dates may not be necessary but nice to know we have timeline coverage
+				notBefore := signer.NotBefore
+				notAfter := signer.NotAfter
+
+				// Put everything together into format strings
+				fmtName := fmt.Sprintf("Name: %s, %s %s", lName, fName, mName)
+				fmtEmail := ""
+				if email != "" {
+					fmtEmail = fmt.Sprintf("Email: %s", email)
+				} else {
+					fmtEmail = fmt.Sprintf("Email: %s", from)
+				}
+				fmtEdiPI := fmt.Sprintf("EDIPI: %s", edipi)
+				fmtUpn := fmt.Sprintf("Principle Name: %s", upn)
+				fmtSerial := fmt.Sprintf("Serial: %x", serial)
+				fmtIssuer := fmt.Sprintf("Issuer: %s", issuer)
+				fmtCA := fmt.Sprintf("Certificate Authority: %s", ca)
+				fmtNotBefore := fmt.Sprintf("Not Before: %s", notBefore)
+				fmtNotAfter := fmt.Sprintf("Not After: %s", notAfter)
+				// Join all the formatted strings
+				fmtSlice := []string{fmtName, fmtEmail, fmtEdiPI, fmtUpn, fmtSerial, fmtIssuer, fmtCA, fmtNotBefore, fmtNotAfter}
+				cert := strings.Join(fmtSlice, "\n")
+				// log.Println(cert)
+
+				// send the cert accross the channel
+				c <- cert
+			}
+		}
+	}
+}
+
 // GetSubFolders is a recursive function which retrieves all sub-folders for the specified folder.
 func GetSubFolders(pstFile pst.File, folder pst.Folder, formatType string, encryptionType string, c chan string) error {
 	subFolders, err := pstFile.GetSubFolders(folder, formatType, encryptionType)
@@ -205,111 +333,13 @@ func GetSubFolders(pstFile pst.File, folder pst.Folder, formatType string, encry
 
 		if len(messages) > 0 {
 			log.Printf("Found %d messages.\n", len(messages))
+			// process messages in parallel
+			mChan := make(chan string)
 			for i, msg := range messages {
 				log.Printf("PROCESSING MESSAGE # %d", i)
-				hasAttachments, err := msg.HasAttachments()
-				if err != nil {
-					return err
-				}
-				if !hasAttachments {
-					continue
-				}
-				from, err := msg.GetFrom(&pstFile, formatType, encryptionType)
-				if err != nil {
-					return err
-				}
-				myAttachments, err := msg.GetAttachments(&pstFile, formatType, encryptionType)
-				if err != nil {
-					return err
-				}
-				for _, attachment := range myAttachments {
-					mimeType, _ := attachment.GetString(14094)
-					if mimeType != "multipart/signed" {
-						continue
-					}
-					attachStream, err := attachment.GetInputStream(&pstFile, formatType, encryptionType)
-					if err != nil {
-						return err
-					}
-					attachBytes, err := attachStream.ReadCompletely()
-					if err != nil {
-						return err
-					}
-					msg, err := mail.ReadMessage(bytes.NewReader(attachBytes))
-					if err != nil {
-						log.Fatal(err)
-					}
-					_, params, err := mime.ParseMediaType(msg.Header.Get("Content-Type"))
-					if err != nil {
-						return err
-					}
-					mr := multipart.NewReader(msg.Body, params["boundary"])
-					eof := false
-					for !eof {
-						p, err := mr.NextPart()
-						if err == io.EOF {
-							eof = true
-							continue
-						}
-						if err != nil {
-							log.Fatal(err)
-						}
-						slurp, err := io.ReadAll(p)
-						if err != nil {
-							log.Fatal(err)
-						}
-						if p.Header.Get("Content-Type") == "application/pkcs7-signature; name=\"smime.p7s\"" {
-							dst := make([]byte, len(slurp))
-							n, err := base64.StdEncoding.Decode(dst, slurp)
-							if err != nil {
-								log.Fatal(err)
-							}
-							dst = dst[:n]
-							p7m, err := pkcs7.Parse(dst)
-							if err != nil {
-								log.Fatal(err)
-							}
-							signer := p7m.GetOnlySigner()
-							cn := signer.Subject.CommonName
-							log.Println(cn)
-							cnSplit := strings.Split(cn, ".")
-							lName := cnSplit[0]
-							fName := cnSplit[1]
-							mName := ""
-							edipi := cnSplit[len(cnSplit)-1]
-							if len(cnSplit) == 4 {
-								mName = cnSplit[2]
-							}
-							email := strings.Join(signer.EmailAddresses, ";")
-							upn := strings.Join(signer.DNSNames, ";")
-							if len(upn) == 0 {
-								upn = fmt.Sprintf("%s@mil", edipi)
-							}
-							serial := signer.SerialNumber
-							issuer := signer.Issuer
-							ca := signer.Issuer.CommonName
-							notBefore := signer.NotBefore
-							notAfter := signer.NotAfter
-							fmtName := fmt.Sprintf("Name: %s, %s %s", lName, fName, mName)
-							fmtEmail := ""
-							if email != "" {
-								fmtEmail = fmt.Sprintf("Email: %s", email)
-							} else {
-								fmtEmail = fmt.Sprintf("Email: %s", from)
-							}
-							fmtEdiPI := fmt.Sprintf("EDIPI: %s", edipi)
-							fmtUpn := fmt.Sprintf("Principle Name: %s", upn)
-							fmtSerial := fmt.Sprintf("Serial: %x", serial)
-							fmtIssuer := fmt.Sprintf("Issuer: %s", issuer)
-							fmtCA := fmt.Sprintf("Certificate Authority: %s", ca)
-							fmtNotBefore := fmt.Sprintf("Not Before: %s", notBefore)
-							fmtNotAfter := fmt.Sprintf("Not After: %s", notAfter)
-							cert := fmt.Sprintf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s", fmtName, fmtEmail, fmtEdiPI, fmtUpn, fmtSerial, fmtIssuer, fmtCA, fmtNotBefore, fmtNotAfter)
-							// log.Println(cert)
-							c <- cert
-						}
-					}
-				}
+				go processMsg(msg, pstFile, formatType, encryptionType, mChan)
+				msgMsg := <-mChan
+				c <- msgMsg
 			}
 		}
 
